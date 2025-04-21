2a2d1557bf5104cfae33cc03f2d69c5de6ece349
hyperon-hyeon <hyperon1122@gmail.com>
2025-04-21 22:57:44 +0900

    inverse image

--- code/inverseImg.cpp
+++ code/inverseImg.cpp
@@ -1,47 +1,48 @@
-#pragma warning(disable:4996) //safety issue cause version upgrade 
+#pragma warning(disable:4996)
 #include <stdio.h>
-#include<stdlib.h> //malloc
-#include <Windows.h> //bitmap header structure
+#include <stdlib.h>
+#include <Windows.h>
+
+void inverseImg(BYTE* Img, BYTE* Out, int W, int H) {
+	int ImgSize = W * H;
+	for (int i = 0; i < ImgSize; i++) Out[i] = 255 - Img[i];
+}
 
 int main() {
-	BITMAPFILEHEADER hf; //14 bytes
-	BITMAPINFOHEADER hInfo; //40 bytes
-	RGBQUAD hRGB[256]; //palette, 256*4=1024bytes
+	BITMAPFILEHEADER hf;
+	BITMAPINFOHEADER hInfo;
+	RGBQUAD hRGB[256];
 	FILE* fp;
-	fp = fopen("lenna.bmp", "rb"); // "rb"=read binary file
+
+	fp = fopen("coin.bmp", "rb");
 	if (fp == NULL) {
-		printf("File not found!\n");
+		printf("File not found\n");
 		return -1;
 	}
-	// 1. file -> memory
-	
-	//fileHeader, infoHeader, palette
-	fread(&hf,sizeof(BITMAPFILEHEADER),1,fp); // file information save memory variable.(saving at fileheader(pointer),14bytes,once reading, atfile pointer)
+
+	fread(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
 	fread(&hInfo, sizeof(BITMAPINFOHEADER), 1, fp);
-	fread(hRGB, sizeof(RGBQUAD), 256, fp);//(array's name is address(not pointer),RGBQuard_4bytes, 256 times, at file pointer)
+	fread(hRGB, sizeof(RGBQUAD), 256, fp);
 
-	//pixel
-	int ImgSize = hInfo.biWidth * hInfo.biHeight; //malloc as ImgSize
-	BYTE * Image= (BYTE*) malloc(ImgSize); // BYTE Image[ImgSize]<-not available, return start point 
+	int W = hInfo.biWidth, H = hInfo.biHeight;
+	int ImgSize = W * H;
+
+	BYTE* Image = (BYTE*)malloc(ImgSize);
 	BYTE* Output = (BYTE*)malloc(ImgSize);
-	fread(Image, sizeof(BYTE), ImgSize, fp);
 
+	fread(Image, sizeof(BYTE), ImgSize, fp);
 	fclose(fp);
 
-	//2. memory -> img process
-	for (int i = 0; i < ImgSize; i++) {
-		Output[i] = 255 - Image[i]; // reverse Img, if Output[i]>255 - overflow (need clipping!)
-	}
+	//Image process
+	inverseImg(Image, Output, W, H);
 
-	//3. save reverse img
-	fp = fopen("output.bmp", "wb");
-	fwrite(&hf, sizeof(BYTE), sizeof(BITMAPFILEHEADER), fp); //1byte(writing binary)*14times
-	fwrite(&hInfo, sizeof(BYTE), sizeof(BITMAPINFOHEADER), fp);
+	fp = fopen("inverse_image.bmp", "wb");
+	fwrite(&hf, sizeof(BITMAPFILEHEADER), 1, fp);
+	fwrite(&hInfo, sizeof(BITMAPINFOHEADER), 1, fp);
 	fwrite(hRGB, sizeof(RGBQUAD), 256, fp);
 	fwrite(Output, sizeof(BYTE), ImgSize, fp);
 	fclose(fp);
 
-	//malloc is not free -> *memory leak* error(heap memory still use)
 	free(Image);
 	free(Output);
 
